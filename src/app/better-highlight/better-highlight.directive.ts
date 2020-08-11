3e709d4f87a7fe26fa5aec7282ed4401a414511f
Marcela Bártová <marcela.bartova@principal.cz>
2020-08-11 16:03:50 +0200

    Section 7: Directives Deep Dive 98. Binding to Directive Properties

--- src/app/better-highlight/better-highlight.directive.ts
+++ src/app/better-highlight/better-highlight.directive.ts
@@ -1,15 +1,19 @@
-import {Directive, ElementRef, HostBinding, HostListener, OnInit, Renderer2} from '@angular/core';
+import {Directive, ElementRef, HostBinding, HostListener, Input, OnInit, Renderer2} from '@angular/core';
 
 @Directive({
   selector: '[appBetterHighlight]'
 })
 export class BetterHighlightDirective implements OnInit {
+  @Input() defaultColor = 'transparent'; /*property binding*/
+  @Input('appBetterHighlight') highlightColor = 'blue';
+  /*bind this from outside - from template app.component.html bind to defaultColor*/
   // tslint:disable-next-line:max-line-length
-  @HostBinding('style.backgroundColor') backgroundColor: string = 'transparent'; /*we can pass a string defining to which property of hosting element we want to bind*/
+  @HostBinding('style.backgroundColor') backgroundColor: string; /*= this.defaultColor;*/ /*we can pass a string defining to which property of hosting element we want to bind*/
 
   constructor(private elRef: ElementRef, private renderer: Renderer2) { }
 
   ngOnInit() {
+    this.backgroundColor = this.defaultColor;
     /*this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'blue');*/
   }
 
@@ -17,11 +21,12 @@ export class BetterHighlightDirective implements OnInit {
   hostlistener takes argument name as a input*/
   @HostListener('mouseenter') mouseover(eventData: Event) {
     /*this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'blue');*/ /*po najeti kurzorem mysi se radek prebarvi na modro*/
-    this.backgroundColor = 'blue';
+    this.backgroundColor = this.highlightColor;
   }
   @HostListener('mouseleave') mouseleave(eventData: Event) {
     /*this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'transparent');*/
-    this.backgroundColor = 'transparent';
+    /*this.backgroundColor = 'transparent';*/
+    this.backgroundColor = this.defaultColor;
   }
 
 }
